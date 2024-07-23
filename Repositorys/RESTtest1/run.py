2d16af4094f81efa6e500a0246a74f9d19af9117
ASK-USE <knueppelalex@web.de>
2024-07-23 10:56:13 +0200

    finally; works

--- Repositorys/RESTtest1/run.py
+++ Repositorys/RESTtest1/run.py
@@ -1,8 +1,15 @@
 # run.py
 
 from app import create_app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/test')
+def test_route():
+    return "Hallo Testsubjekt"
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run()
